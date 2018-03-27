0fbf659465b68997126fb5f622e2c35d18edf335
happypeppy <37802816+happypeppy@users.noreply.github.com>
2018-03-27 17:34:50 +0300

    Cache Matrix and Cache Solve
    This script contains the scripts necessary to pass Dr. Peng's Programming Assignment 2 from the R programming course in Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,13 @@
 ##################### Cache Matrix and Cache Solve ############################
 ###############################################################################
 
+# The first function, makeMatrix creates a special "matrix",
+# which is really a list containing a function to
+# 
+# set the value of the matrix
+# get the value of the matrix
+# set the value of the inverse
+# get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
@@ -18,7 +25,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+# The following function calculates the inverse of the special "matrix" created 
+# with the above function. However, it first checks to see if the inverse has 
+# already been calculated. If so, it gets the inverse from the cache and skips the 
+# computation. Otherwise, it calculates the inverse of the data and sets the value 
+# of the inverse in the cache via the setinverse function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -39,6 +50,9 @@ cacheSolve <- function(x, ...) {
 M <- matrix(runif(9),3,3)
 M
 Mc <-  makeCacheMatrix(M)
+cacheSolve(Mc)
+
+
 cacheSolve(Mc)
 
 
